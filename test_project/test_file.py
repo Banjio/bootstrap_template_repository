d6fc4dea74bb70553c26e85b216371b57f7c48c7
Banjio <beiermaximilian13@gmail.com>
2024-07-16 23:27:03 +0200

    Removed old history

--- test_project/test_file.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as np
-from pydantic import BaseModel
-
-def this_is_a_test_function(a, b):
-    if isinstance(a, int) or isinstance(b, int): # This comment should resemble a way to long line so+
-        return a + b
-    else:
-        return int(a) + int(b)
